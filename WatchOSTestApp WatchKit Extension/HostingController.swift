9c855c8d469e75f75007f7c7a0f1c0830bfff05f
SRoller <sroller@justfab.com>
2019-06-05 13:53:06 -0700

    Initial Commit

---
+++ WatchOSTestApp WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchOSTestApp WatchKit Extension
+//
+//  Created by Shawn Roller on 6/5/19.
+//  Copyright © 2019 Shawn Roller. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController : WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WatchOSTestApp WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  WatchOSTestApp WatchKit Extension
+//
+//  Created by Shawn Roller on 6/5/19.
+//  Copyright © 2019 Shawn Roller. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews : PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
