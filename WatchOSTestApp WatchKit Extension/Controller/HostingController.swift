275d5005715fcfacc563c3196a305aa8242b6150
SRoller <sroller@justfab.com>
2019-06-25 14:54:22 -0400

    Adds initial bluetooth support.

--- WatchOSTestApp WatchKit Extension/Controller/HostingController.swift
+++ WatchOSTestApp WatchKit Extension/Controller/HostingController.swift
@@ -9,16 +9,24 @@
 import WatchKit
 import Foundation
 import SwiftUI
+import CoreBluetooth
 
 class HostingController : WKHostingController<LoginView> {
     
     let UI_TESTING = true
+    
     var dataManager: DataManagerProtocol!
+    
+    // Bluetooth
+    let scannerCBUUID = CBUUID(string: "0x1813")
+    var centralManager: CBCentralManager!
+    var scannerPeripheral: CBPeripheral!
     
     override func willActivate() {
         super.willActivate()
         
         self.dataManager = self.UI_TESTING ? MockDataManager() : DataManager()
+//        self.setupBluetooth()
     }
     
     override var body: LoginView {
@@ -52,4 +60,110 @@ extension HostingController: LoginDelegate {
         
         self.dataManager.login(with: username, and: password, completion: loginCompletion)
     }
+}
+
+// MARK: - Bluetooth
+extension HostingController: CBCentralManagerDelegate {
+    
+    func setupBluetooth() {
+        
+        self.centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+    }
+    
+    func scanForPeripherals() {
+        
+//        self.centralManager.scanForPeripherals(withServices: self.scannerCBUUID)
+        self.centralManager.scanForPeripherals(withServices: nil)
+        
+    }
+    
+    private func scan(from characteristic: CBCharacteristic) -> String {
+        guard let characteristicData = characteristic.value else { return "" }
+        let byteArray = [UInt8](characteristicData)
+        
+        let firstBitValue = byteArray[0] & 0x01
+        if firstBitValue == 0 {
+            return byteArray[1]
+        } else {
+            return byteArray[1] << 8 + byteArray[2]
+        }
+    }
+    
+    private func onScanReceived(_ scan: String) {
+        print("Scanned: \(scan)")
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        
+        switch central.state {
+            case .unknown:
+                print("central.state is .unknown")
+            case .resetting:
+                print("central.state is .resetting")
+            case .unsupported:
+                print("central.state is .unsupported")
+            case .unauthorized:
+                print("central.state is .unauthorized")
+            case .poweredOff:
+                print("central.state is .poweredOff")
+            case .poweredOn:
+                print("central.state is .poweredOn")
+                self.scanForPeripherals()
+        }
+        
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print(peripheral)
+        self.scannerPeripheral = peripheral
+        self.scannerPeripheral.delegate = self
+        self.centralManager.stopScan()
+        self.centralManager.connect(self.scannerPeripheral)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("connected!")
+        self.scannerPeripheral.discoverServices(nil)
+    }
+    
+}
+
+extension HostingController: CBPeripheralDelegate {
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
+        for service in services {
+            print(service)
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        guard let characteristics = service.characteristics else { return }
+        for characteristic in characteristics {
+            print(characteristic)
+            
+            if characteristic.properties.contains(.read) {
+                print("\(characteristic.uuid) containes read")
+                peripheral.readValue(for: characteristic)
+            }
+            if characteristic.properties.contains(.notify) {
+                print("\(characteristic.uuid) containes notify")
+                peripheral.setNotifyValue(true, for: characteristic)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        switch characteristic.uuid {
+            case self.scannerCBUUID:
+                let scan = self.scan(from: characteristic)
+                self.onScanReceived(scan)
+                print(characteristic.value ?? "no value")
+            default:
+                print("unhandled characteristic, UUID: \(characteristic.uuid)")
+        }
+    }
+    
 }
